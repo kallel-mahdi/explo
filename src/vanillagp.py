Fixed backpropagation error for linear case

src/vanillagp.py
@@ -25,8 +25,8 @@ def step(model,objective_env):
       acq_function=EI,
       bounds=torch.tensor([[0.0] * len_params, [1.0] * len_params]),
       q=1,
-      num_restarts=3,
-      raw_samples=5,
+      num_restarts=5,
+      raw_samples=7,
       options={},
     )
 