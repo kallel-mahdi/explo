245e26ad794a47a54cf73a058d1aee2e131c480d
kallel-mahdi <kallel@eurecom.fr>
2022-05-10 18:15:24 +0200

    Fitting gp is working, we need to include theta in the gradient graph

--- src/vanillagp.py
+++ src/vanillagp.py
@@ -25,8 +25,8 @@ def step(model,objective_env):
       acq_function=EI,
       bounds=torch.tensor([[0.0] * len_params, [1.0] * len_params]),
       q=1,
-      num_restarts=20,
-      raw_samples=100,
+      num_restarts=3,
+      raw_samples=5,
       options={},
     )
 
