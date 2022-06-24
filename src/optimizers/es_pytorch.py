0e8ebc6c1fdc1c0675cf32f075bfa0ebf9cd4c5b
kallel-mahdi <kallel@eurecom.fr>
2022-06-24 13:08:02 +0200

    We have a functioning implementation of ES that uses pytorch, now to integrate mushroomrl mdp into our code

--- src/optimizers/es_pytorch.py
+++ src/optimizers/es_pytorch.py
@@ -1,66 +1,108 @@
+import multiprocessing as mp
+from copy import deepcopy
 
-import gym
-import numpy as np
 import torch
-from src.environment import EnvironmentObjective
-from src.policy import MLPSequential
 
 
-class Runner:
+class ESOptimizer(object):
     
     
-    def run(self,params):
+    def __init__(self,env,policy_params,
+                 params_per_step,episodes_per_param,n_workers,
+                 sigma):
         
+        self.__dict__.update(locals())
         
-
-class BasicNumGrad:
-    
-    def __init__(self, env, policy,policy_params):
-        
-        self.env_obj = EnvironmentObjective(env, policy)
-        self.policy_params = policy_params
         self.optimizer = torch.optim.Adam([self.policy_params])
         
 
-    def approx_grad(self, sigma=1e-1, roll_per_parms=10, param_per_step=10):
+    def run_noisy_params(self,env,policy_params,
+                        seed,sigma,episodes_per_param):
         
-        policy_params = self.policy_params
-        all_e, all_f = [], []
+        """ Generate and run a pair of symmetric noisy parameters
+
+        Returns:
+            weighted_noise: J(theta+eps)*eps
+        """
         
-        for k in range(param_per_step):
-            
-            all_e.append(torch.randn_like(policy_params) * sigma)
-            self.env_obj.mlp.set_weights(policy_params + all_e[-1])
-            all_f.append(self.env_obj.run_many(None, roll_per_parms)[0].item())
+        ## Generate noisy parameters
+        ## Noise is different for each seed
+        torch.manual_seed(seed)
+        eps = torch.randn_like(policy_params) * sigma
+        
+        params1 = policy_params + eps
+        params2 = policy_params - eps ## symmetric noise
 
-            # mirror perturbation
-            all_e.append(-all_e[-1])
-            self.env_obj.mlp.set_weights(policy_params + all_e[-1])
-            all_f.append(self.env_obj.run_many(None, roll_per_parms)[0].item())
+        tmp_env = deepcopy(env)
+        rewards1,states1 = tmp_env.run_many(params1,episodes_per_param)
+        rewards2,states2 = tmp_env.run_many(params2,episodes_per_param)
+        
+        weighted_noise = (1/sigma)*(rewards1*eps - rewards2*eps)*0.5 ## 
+        
+        return weighted_noise
 
-        nb_evals = len(all_f)
 
-        # weights = np.asarray(all_f)
-        # weights = (weights - weights.mean())  # also seems to do ok
 
-        weights = np.argsort(all_f).argsort() / (nb_evals - 1) - .5
+    def compute_gradient(self,env,policy_params,sigma,
+                    params_per_step,episodes_per_param,n_workers=None):
 
-        return sum([w * e for w, e in zip(weights, all_e)]) / sigma / nb_evals, mu, all_f
+        args = [(env,policy_params,i,1,episodes_per_param) for i in range(params_per_step)]
+        
+        # Step 1: Init multiprocessing.Pool()
 
-    def optimize(self, step_size=1e-3, nb_steps=100, **approx_grad_kwargs):
-        for k in range(nb_steps):
-            grad, old_par, evals = self.approx_grad(**approx_grad_kwargs)
-            print(f'iteration {k}, mean {sum(evals)/len(evals)} max {max(evals)}')
-            adam_grad = self.adamize(grad, k + 1)
-            self.env_obj.mlp.set_weights(old_par + step_size * adam_grad)
+        if n_workers is None : n_workers = mp.cpu_count()
+        pool = mp.Pool(n_workers)
 
+        # Step 2:  Run processes (we might need to use mapreduce to avoid big memory usage)
+        weighted_noises = pool.starmap(self.run_noisy_params,args) ## list of [(reward*eps)]
+
+        # Step 3: Wait for workers to run then close pool
+        pool.close()
+        pool.join()
+
+        gradient  = torch.vstack(weighted_noises).mean(dim=0)
+        
+        return gradient
+    
+    
+    def step(self):
+        
+        
+        policy_grad = self.compute_gradient(self.env,self.policy_params,self.sigma,
+                    self.params_per_step,self.episodes_per_param,)
+    
+        self.optimizer.zero_grad()        
+        self.policy_params.grad = -policy_grad ## optimizer usually minimizes
+        self.optimizer.step()
+        
+        return policy_grad
+        
+        
+    
 
 if __name__ == '__main__':
-    torch.set_num_threads(1)
-    # env = gym.make('Pendulum-v1')
-    env = gym.make('Walker2DBulletEnv-v0')
-    mlp = MLPSequential([env.observation_space.shape[0], 64, 64, env.action_space.shape[0]])
+    
+ 
+    from src.optimizers.es_pytorch import ESOptimizer
+    from src.helpers import setup_experiment
+    from src.config import get_configs
+    import torch
 
-    # num grad
-    optimizer = BasicNumGrad(env, mlp)
-    optimizer.optimize(step_size=1e-3, nb_steps=500, sigma=2e-2, roll_per_parms=10, param_per_step=10)
+    env_name = "Swimmer-v4"
+    kernel_name = "rbf"
+
+    env_config,likelihood_config,kernel_config,optimizer_config,trainer_config = get_configs(env_name,kernel_name)
+    _,env = setup_experiment(env_config,kernel_config,likelihood_config,additional_layers=[])
+
+    optimizer = ESOptimizer(env,torch.zeros(18),sigma=1e-2,
+                    params_per_step=50,episodes_per_param=1,n_workers=8)
+
+
+    for i in range(100):
+        
+        optimizer.step()
+        
+        if i % 3 == 0:
+            avg_reward,_ = env.run_many(optimizer.policy_params,5)
+            print(f'avg_rewarad {avg_reward} ')
+            print(f'policy_params : {optimizer.policy_params}')
